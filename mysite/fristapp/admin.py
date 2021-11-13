431750ada881e4b0727acd229dc33f964c54069d
codetext <lenhan16587@gmail.com>
2021-11-13 16:28:46 +0700

    DJANGO
    ket noi data

--- mysite/fristapp/admin.py
+++ mysite/fristapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Website,Test
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Website)
+admin.site.register(Test)
--- mysite/fristapp/models.py
+++ mysite/fristapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=150, unique=True)
+    def __str__(self):
+        return self.name
+
+class Website(models.Model):
+    category=models.ForeignKey(Category, on_delete=models.PROTECT)
+    name=models.CharField(max_length=250, unique=True)
+    url=models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+class Test(models.Model):
+    ma_so=models.AutoField(primary_key=True)
+    ten=models.CharField(max_length=150, unique=True)
+    def __str__(self):
+        return self.name
