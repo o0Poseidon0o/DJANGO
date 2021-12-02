260ff236f62e882aee88910555ced126c08814f8
o0Poseidon0o <90851953+o0Poseidon0o@users.noreply.github.com>
2021-12-02 10:35:33 +0700

    DJANGO
    Estore

--- Estore/Estore/asgi.py
+++ Estore/Estore/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Estore project.
+ASGI config for EStore project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Estore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EStore.settings')
 
 application = get_asgi_application()
