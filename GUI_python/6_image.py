31f348d6983ae4c4c46b9e414804aeaf0ecefa8f
o0Poseidon0o <90851953+o0Poseidon0o@users.noreply.github.com>
2022-03-17 12:15:33 +0700

    DJANGO
    CHANGE

--- GUI_python/6_image.py
+++ GUI_python/6_image.py
@@ -1,11 +1,11 @@
 from tkinter import *
-from PIL import ImageTk,Image
+from PIL import ImageTk,Image #Phai cai Pillow. pip install pillow
 
 root = Tk()
 root.title("Test thoat chuong trinh")
-root.iconbitmap('D:\django\DJANGO_KHTN\DJANGO\GUI_python\icon.png')
+root.iconbitmap('C:\Users\POSEIDON\Desktop\DJANGO\DJANGO\image\icon.png')
 
-my_img = ImageTk.PhotoImage(Image.open("gai.jpg"))
+my_img = ImageTk.PhotoImage(Image.open('image\doreamon.png'))
 my_label = Label(image=my_img)
 my_label.pack()
 
